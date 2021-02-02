af2f631c971e138bc2161af9c67f3395be4c7e46
Sergey Smolnikov <ssmol@equinor.com>
2021-02-02 21:37:28 +0100

    Code

---
+++ server/start-servers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for PORT in {32001..32005}
+  do
+    echo $PORT
+    python3 -m http.server $PORT &
+    echo "started ${PORT}";
+done
+
+while true
+ do
+   for PORT in {32001..32005}
+    do
+      echo "Get port ${PORT}"
+      curl "http://localhost:${PORT}"
+      sleep 1
+    done
+ done
